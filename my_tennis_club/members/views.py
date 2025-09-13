037d402618189271b0d1cd6b8f38a569d9fff96a
hiniken5 <hinikengonzalez@gmail.com>
2025-09-13 13:38:08 -0400

    Feat: Django - Agregar archivo estático

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -10,8 +10,8 @@ def members(request):
   }
   return HttpResponse(template.render(context, request))
   
-def details(request, id):
-  mymember = Member.objects.get(id=id)
+def details(request, slug):
+  mymember = Member.objects.get(slug=slug)
   template = loader.get_template('details.html')
   context = {
     'mymember': mymember,
@@ -23,28 +23,8 @@ def main(request):
   return HttpResponse(template.render())
 
 def testing(request):
-  mymembers = Member.objects.all().values()
-  mydata = Member.objects.all()
-  mydata2 = Member.objects.values_list('firstname', flat = True)
-  mydata3 = Member.objects.filter(firstname='Juana').values()
-  mydata4 = Member.objects.filter(lastname='Perez', id=3).values()
-  mydata5 = Member.objects.filter(firstname__startswith='M').values;
   template = loader.get_template('template.html')
   context = {
-    'fruits': ['Apple', 'Banana', 'Cherry'],
-    'firstname': 'Linus',
-    'mymembers': mymembers,
-    'mymembers2': mydata,
-    'mymembers3': mydata2,
-    'mymembers4': mydata3,
-    'mymembers5': mydata4,
-    'mymembers6': mydata5,
-    'greeting' : 0,  
-    'x': ['Apple', 'Banana', 'Cherry'], 
-    'y': ['Apple', 'Banana', 'Cherry'],
-    'cars': [{"model": "Apple", "brand": "Banana", "year": 2}, 
-             {"model": "Apple", "brand": "Apple", "year": 3}, 
-             {"model": "Banana", "brand": "Apple", "year": 4}]
+    'fruits': ['Apple', 'Banana', 'Cherry'],   
   }
   return HttpResponse(template.render(context, request))
-
